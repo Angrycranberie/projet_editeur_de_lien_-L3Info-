10dd7c9f44012893a504197e13d2b3bc34c96a8e
Alexis YVON <alexis.yvon@gmail.com>
2019-12-19 16:16:51 +0100

    e5m1c004 # Harmonisation & corrections
    - Harmonisation des noms des fichiers pour expliciter leur but, éviter leur écrasement malencontreux et permettre un usage universel
    - Remplacement de 'entete.c/h' par son équivalent optimisé 'header.c/h' (anciennement 'entete_modularise.c/h')
    - Remplacement des valeurs brutes (0, 1, etc) par les constantes correspondantes (EI_MAG0, etc) dans les fichiers 'readelf_*.c'

--- elf_linker-1.0/readelf_header.c
+++ elf_linker-1.0/readelf_header.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#include "entete_modularise.h"
-#include "elf.h"
-// Donne les informations sur le header d'un fichier elf pass� en argument.
-int main (int argc, char ** argv){
-    if (argc!=2) {
+#include <elf.h>
+#include "header.h"
+
+// Donne les informations sur le header d'un fichier ELF passé en argument.
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         printf("Erreur: Précisez un nom de fichier, et un seul\n");
         return 1;
     }
-    FILE * f;
-    f = fopen(argv[1],"r");
-    if (f==NULL){
+    FILE *f;
+    f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
         printf("Erreur lors de l'ouverture en lecture du fichier\n");
         return 1;
     }
